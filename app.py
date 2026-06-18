64dc3327e145484e09d7f487c5446402aa36c9ba
epqlffltm <kjh55984249@gmail.com>
2026-06-18 12:11:03 +0900

    fastAPI 첫커밋

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {"hallow" : "wared"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
